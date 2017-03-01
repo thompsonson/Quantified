84d09b0f4278d3571265554684744690d1e09ac1
Matt Thompson <thompsonson@gmail.com>
2017-03-01 10:35:23 +0000

    check in before shelving as I have found out human api is 000/month...

--- Quantified/settings.py
+++ Quantified/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'core.apps.CoreConfig',
     'withingsapp',
+    'django_humanapi',
     'storages',
     'django_dropbox',
     'django.contrib.admin',
